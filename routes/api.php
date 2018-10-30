39a5bf872d87cd30b5d3957cbc22659bc60472a6
Ahmed <ahmedkappo7@gmail.com>
2018-10-30 15:43:19 -0700

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/products', 'ProductController');
+
+//products/22/reviews
+
+Route::group(['prefix' => 'products'], function() {
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+});
